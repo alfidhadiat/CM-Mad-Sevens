3900c5e7c757087ee25a6dd1f730dcf56df157c1
M.E. Kopmels <s3147800@ai178139.ai.rug.nl>
2021-02-24 14:08:44 +0100

    Created the XCode project

---
+++ MADSevens/MADSevens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MADSevens
+//
+//  Created by M.E. Kopmels on 24/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
