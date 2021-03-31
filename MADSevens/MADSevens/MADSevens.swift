8c3c15f29526dfc1fd316bbd7df1aa1701657e94
Marieke <marieke@kopmels.com>
2021-03-31 17:48:04 +0200

    getting the act to work, still working on nil error

--- MADSevens/MADSevens/MADSevens.swift
+++ MADSevens/MADSevens/MADSevens.swift
@@ -11,11 +11,12 @@ class MADSevens {
     
     private var deck: Deck
     private var currentPlayer: Player
-//    private var model: ACTRModel
+    private var actr: sevensACTR
     
     init() {
         print("Intitializing MADSevens game (INIT OF MADSevens)")
         deck = Deck()
+        actr = sevensACTR(actrName: "sevens")
         currentPlayer = Player.player
         printGame()
     }
@@ -32,19 +33,20 @@ class MADSevens {
         deck.drawCard(player: currentPlayer)
     }
     
-    func playCard(card: Card, newSuit: Suit?) -> Bool {
+    func playCard(card: Card, newSuit: Suit?) {
         if deck.legalMove(card: card) {
             deck.playCard(card: card, player: currentPlayer, newSuit: newSuit)
-            return true
-        }
-        return false
-    }
+        } else {
+            drawCard()
+            print("Invalid move, a card is drawn for you!")
 
+        }
+    }
+    
     func modelTurn() {
         //Here, the model is called, and afterwards his move is executed
         //E.g. decide to draw a card:
-        print("Model draws a card")
-        drawCard()
+        actrTurn()
         printGame()
         passTurn()
     }
@@ -74,9 +76,9 @@ class MADSevens {
         return "None"
     }
     
-//    func done() -> Bool {
-//        return finished
-//    }
+    //    func done() -> Bool {
+    //        return finished
+    //    }
     
     func legalMove(card: Card) -> Bool {
         return deck.legalMove(card: card)
@@ -118,6 +120,10 @@ class MADSevens {
         return deck.getModelHand()
     }
     
+    func rememberSuitRank() {
+        actr.rememberSuitRank(topCard: deck.getTopDiscardCard())
+    }
+    
     func printGame() {
         print("Current situation:")
         print("Current player: \(currentPlayer)")
@@ -133,4 +139,137 @@ class MADSevens {
         }
         print("\n Top of the discard pile: \(deck.getTopDiscardCard().getSuit()) - \(deck.getTopDiscardCard().getRank())")
     }
+    
+    /*************************
+     The procedure for the ACT-R's turn
+     *************************/
+    func actrTurn() {
+        
+        // First, put current ACTR hand and top discard into ACTR
+        actr.setHand(newHand: getPlayerHand())
+        
+        // Go through the model's turn to generate a "choice"
+        let choice = actr.turn(topCard: getTopDiscardCard())
+        
+        // Switch cases that play out the model's choice
+        switch choice {
+        
+        // When ACT-R has no legal hand, the choice is "draw"
+        case "draw":
+            drawCard()
+            
+        // When ACT-R has only one legal, the choice is "playOne"
+        case "playOne":
+            for card in actr.getHand() {
+                if deck.legalMove(card: card) == true {
+                    let onlyLegal = card
+                    let onlyLegalRank = onlyLegal.getRank()
+                    var newSuit: Suit? = nil
+                    if onlyLegalRank == Rank.VII {
+                        actr.predictSuit()
+                        newSuit = actr.getPredictedSuit()
+                    }
+                    playCard(card: onlyLegal, newSuit: newSuit)
+                }
+            }
+            
+        // When ace is legal against a two, choice is "playAce"
+        case "playAce":
+            var aceCard: Card
+            for card in actr.getHand() {
+                let rank = card.getRank()
+                if rank == Rank.Ace {
+                    aceCard = card
+                    playCard(card: aceCard, newSuit: nil)
+                }
+            }
+            
+        // When choice is "prediction", try play a card with the suit
+        case "prediction":
+            let predictSuit = actr.getPredictedSuit()
+            var suitMatched = "no"
+            var cardToPlay: Card
+            
+            for card in actr.getHand() {
+                let isLegal = deck.legalMove(card: card)
+                let cardSuit = card.getSuit()
+                if cardSuit == predictSuit && isLegal == true {
+                    cardToPlay = card
+                    suitMatched = "yes"
+                }
+            }
+            if suitMatched == "no" {
+                for card in actr.getHand() {
+                    let isLegal = deck.legalMove(card: card)
+                    if isLegal == true {
+                        cardToPlay = card
+                        playCard(card: cardToPlay, newSuit: nil)
+                    }
+                }
+            }
+            
+        // When not against two and legal option with more than one count,
+        // it can only be a rank, so choice is "bestRank"
+        case "bestRank":
+            let bestRank = actr.getBestRank()
+            var cards = [Card]()
+            for card in actr.getHand() {
+                let rank = card.getRank()
+                if rank == bestRank {
+                    cards.append(card)
+                    playCard(card: cards[0], newSuit: nil)
+                }
+            }
+            
+        // !!! Need a game method that plays multiple rank cards
+        
+        // If II played and no ace available, choice is "checkAce"
+        case "checkAce":
+            // If predict slot is not nil, check if ace suit is in hand
+            // TODO: We assume this returns a "nil" if no ace has been played yet
+            var playTwo: Card
+//            var safeTwo: Card
+            
+            if actr.getLastAction() != nil {
+                let aceSuit = suitStringToSuit(suitString: actr.getLastAction()!)
+//                var twoExists = "no"
+                for card in actr.getHand() {
+                    let cardSuit = card.getSuit()
+                    let cardRank = card.getRank()
+                    if (cardRank == Rank.Ace && cardSuit == aceSuit) {
+                        playTwo = card
+                        playCard(card: playTwo, newSuit: nil)
+//                        twoExists = "yes"
+                    } else {
+                        for card in actr.getHand() {
+                            let cardRank = card.getRank()
+                            if (cardRank == Rank.II) {
+                                playTwo = card
+                                playCard(card: playTwo, newSuit: nil)
+                            }
+                        }
+                    }
+                }
+                // If a II in hand matches suit with ace, play it
+                // Else if no II matches suit, play a random II
+//                if twoExists == "yes" {
+//                    playTwo = safeTwo
+//                } else if twoExists == "no" {
+//
+//                }
+            } else {
+                for card in actr.getHand() {
+                    let cardRank = card.getRank()
+                    if cardRank == Rank.II {
+                        playTwo = card
+                        playCard(card: playTwo, newSuit: nil)
+                    }
+                }
+            }
+        default:
+            break
+        }
+        passTurn()
+    }
+    
 }
--- MADSevens/MADSevens/sevensACTR.swift
+++ MADSevens/MADSevens/sevensACTR.swift
@@ -1,241 +1,274 @@
 // Swift class of ACT-R model for Sevens
 
-import Model
+//import Model
 
-class actr {
+class sevensACTR {
 
     // --------------------
     // stored variables
     // --------------------
-    var model
-    var legals: String
-	var twoState: String
-	var aceHand: String
-	var choice: String
-	var top: Card
-	var hand: [Card]
+    private var model: Model
+    private var legals = ""
+    private var twoState = ""
+    private var aceHand = ""
+    private var choice = ""
+    private var hand = [Card]()
 
     // --------------------
     // Initialize Function
     // --------------------
     init(actrName: String) {
-        self.model = Model(actrName)
-    }    
+        model = Model()
+        model.loadModel(fileName: actrName)
+    }
 
     // --------------------
     // Functions
     // --------------------
-	
-	// Function that runs the model's turn
-	func turn() {
+    
+    // Function that runs the model's turn
+    func turn(topCard: Card) -> String {
 
-		// Go through hand in case an ace exists
-		checkHandForAce()
-		
-		// Count the legal moves in ACT-R's hand
-		self.legals = countLegals()
-		
-		// Add legal string into ACT-R's goal buffer as a state
-		model.buffers["goal"]!.setSlot("state", self.legals)
-		
-		// Run the model until it finds an action
-		model.run()
+        // Go through hand in case an ace exists
+        checkHandForAce()
+        
+        // Check if top was ace
+        if topCard.getRank() == Rank.Ace {
+            storeAce(topCard: topCard)
+        }
+        
+        // Count the legal moves in ACT-R's hand
+        self.legals = countLegals(topCard: topCard)
+        
+        // Add legal string into ACT-R's goal buffer as a state
+        if model.buffers["goal"] == nil {
+            model.run()
+            print("\(String(describing: model.buffers["goal"]))")
+            model.buffers["goal"]!.setSlot(slot: "state", value: self.legals)
+        } else {
+            model.buffers["goal"]!.setSlot(slot: "state", value: self.legals)
+        }
+        
+        // Run the model until it finds an action
+        model.run()
 
-		// Perform the ACT-R's action
-		self.choice = model.lastAction("choice")
-		switch self.choice {
-		case "draw":
-			return self.choice 
-		case "playOne":
-			return self.choice
-		case "multipleCheck":
-			self.choice = multipleLegals()
-			return self.choice
-		}
-	}
-	
-	// Goes through the hand to check for aces
-	func checkHandForAce() {
-		for card in actr.hand {
-			let rank = card.getRank().rawValue
-			if rank == "Ace" {
-				let suit = card.getSuit().rawValue
-				var aceSuit = "ace" + suit
-				aceChunk = Chunk(aceSuit, model)
-				aceChunk.slotvals["isa"] = "discardedAce"
-				aceChunk.slotvals["aceSuit"] = suit
-				self.model.dm.addToDm(aceChunk)
-			}
-		}
-	}
+        // Perform the ACT-R's action
+        self.choice = model.lastAction(slot: "choice")!
+        switch self.choice {
+        case "draw":
+            return self.choice
+        case "playOne":
+            return self.choice
+        case "multipleCheck":
+            self.choice = multipleLegals(topCard: topCard)
+            return self.choice
+        default:
+            //TODO: ALfid, something went wrong
+            print("ACTR was unable to make a choice")
+        }
+        return "INVALID" //TODO: is this what we want to do?
+    }
+    
+    // Goes through the hand to check for aces
+    func checkHandForAce() {
+        for card in hand {
+            let rank = card.getRank().rawValue
+            if rank == "Ace" {
+                let suit = card.getSuit().rawValue
+                let aceSuit = "ace" + suit
+                let aceChunk = Chunk(s: aceSuit, m: model)
+                aceChunk.setSlot(slot: "isa", value: suit)
+                model.dm.addToDM(aceChunk)
+            }
+        }
+    }
 
-	// Function that counts legal cards in ACT-R hand
-    func countLegals() -> legals:String{
-		
-		let topSuit = top.getSuit().rawValue
-		let topRank = top.getRank().rawValue
-		var legal = 0
+    // Function that counts legal cards in ACT-R hand
+    func countLegals(topCard: Card) -> String{
+        
+        let topSuit = topCard.getSuit()
+        let topRank = topCard.getRank()
+        var legal = 0
 
         // Seperate procedure between Two and Non-Two
-        if topRank == "II" {
-			
-			// Set state of two in model
-			self.twoState = "yes"
+        if topRank ==  Rank.II {
+            
+            // Set state of two in model
+            self.twoState = "yes"
 
-			// Assume no legal ace in hand yet
-			self.aceHand = "no"
-			
-			// Count all 2's and matching ace in ACT-R's hand
-			for card in actr.hand {
-				let rank = card.getRank().rawValue
-				let suit = card.getSuit().rawValue
-				
-				if rank == "II" {
-					legal += 1
-				} else if rank == "Ace" && suit == topSuit{
-					legal += 1
-					self.aceHand = "yes"
-				}
-			}	
+            // Assume no legal ace in hand yet
+            self.aceHand = "no"
+            
+            // Count all 2's and matching ace in ACT-R's hand
+            for card in hand {
+                let rank = card.getRank()
+                let suit = card.getSuit()
+                
+                if rank == Rank.II {
+                    legal += 1
+                } else if rank == Rank.Ace && suit == topSuit {
+                    legal += 1
+                    self.aceHand = "yes"
+                }
+            }
 
-		} else {
-			
-			// Set state of two and ace in model
-			self.twoState = "no"
-			self.aceHand = "no"
+        } else {
+            
+            // Set state of two and ace in model
+            self.twoState = "no"
+            self.aceHand = "no"
 
-			// Count all cards that match in suit and rank of top card
-			for card in actr.hand {
+            // Count all cards that match in suit and rank of top card
+            for card in hand {
 
-				let rank = card.getRank().rawValue
-				let suit = card.getSuit().rawValue
+                let rank = card.getRank()
+                let suit = card.getSuit()
 
-				if rank == topRank {
-					legal += 1
-				} else if suit == topSuit {
-					legal += 1
-				}
-			}
-		}
-		
-		// Turn count of legals into string categories
-		if legal < 1 {
-			return "noLegal"
-		} else if legal == 1 {
-			return "oneLegal"
-		} else if legal > 1 {
-			return "multipleLegal"
-		}
+                if rank == topRank {
+                    legal += 1
+                } else if suit == topSuit {
+                    legal += 1
+                }
+            }
+        }
+        
+        // Turn count of legals into string categories
+        if legal == 1 {
+            return "oneLegal"
+        } else if legal > 1 {
+            return "multipleLegal"
+        }
+        return "noLegal"
     }
-	
-	// Function that runs when ACT-R must predict a suit
-	func predictSuit() -> suitPrediction: String {
-		
-		// Set goal buffer state as predictSuit
-		model.buffers["goal"]!.setSlot("state", "predictSuit")
+    
+    // Function that runs when ACT-R must predict a suit
+    func predictSuit() {
+        
+        // Set goal buffer state as predictSuit
+        model.buffers["goal"]!.setSlot(slot: "state", value: "predictSuit")
 
-		// Run the model to get the prediction
-		model.run()
+        // Run the model to get the prediction
+        model.run()
 
-		// Return the prediction
-		return model.lastAction("predict")
-	}
+    }
 
-	// When player draws, remember the top card's suit and rank
-	func rememberSuitRank() {
-		let topRank = top.getRank().rawValue
-		let topSuit = top.getSuit().rawValue
-		let suitName = "pred" + topSuit
-		let rankName = "pred" + topRank
+    // When player draws, remember the top card's suit and rank
+    func rememberSuitRank(topCard: Card) {
+        let topRank = topCard.getRank()
+        let topSuit = topCard.getSuit()
+        let suitName = "pred" + topSuit.rawValue
+        let rankName = "pred" + topRank.rawValue
 
-		var suitChunk = Chunk(suitName, self.model)
-		suitChunk.setSlot(slot: "isa", value: "prediction")
-		suitChunk.setSlot(slot: "specific", value: "suit")
-		suitChunk.setSlot(slot: "suitRank", value: topSuit)
+        let suitChunk = Chunk(s: suitName, m: self.model)
+        suitChunk.setSlot(slot: "isa", value: "prediction")
+        suitChunk.setSlot(slot: "specific", value: "suit")
+        suitChunk.setSlot(slot: "suitRank", value: topSuit.rawValue)
 
-		var rankChunk = Chunk(rankName, self.model)
-		rankChunk.setSlot(slot: "isa", value: "prediction")
-		rankChunk.setSlot(slot: "specific", value: "rank")
-		rankChunk.setSlot(slot: "suitRank", value: topRank)
+        let rankChunk = Chunk(s: rankName, m: self.model)
+        rankChunk.setSlot(slot: "isa", value: "prediction")
+        rankChunk.setSlot(slot: "specific", value: "rank")
+        rankChunk.setSlot(slot: "suitRank", value: topRank.rawValue)
+        
+        model.dm.addToDM(suitChunk)
+        model.dm.addToDM(suitChunk)
+    }
 
-		self.model.dm.addToDm(suitChunk)
-		self.model.dm.addToDm(rankChunk)
-	}
+    // Function that generates a chunk of an ace if it has been used
+    func storeAce(topCard: Card) {
 
-	// Function that generates a chunk of an ace if it has been used
-	func storeAce() {
+        // Double check if top really was ace
+        let topRank = topCard.getRank()
+        if topRank == Rank.Ace {
+            
+            // Generate chunk of ace that holds the ace's suit
+            let topSuit = topCard.getSuit()
+            let aceSuit = "ace" + topSuit.rawValue
+            let aceChunk = Chunk(s: aceSuit, m: model)
+            aceChunk.setSlot(slot: "isa", value: "discardedAce")
+            aceChunk.setSlot(slot: "aceSuit", value: topSuit.rawValue)
 
-		// Double check if top really was ace
-		let topRank = top.getRank().rawValue
-		if topRank == "Ace" {
-			
-			// Generate chunk of ace that holds the ace's suit
-			let topSuit = top.getSuit().rawValue
-			let aceSuit = "ace" + topSuit
-			var aceChunk = Chunk(aceSuit, model)
-			aceChunk.setSlot(slot: "isa", value: "discardedAce")
-			aceChunk.setSlot(slot: "aceSuit", value: topSuit)
+            // Add ace chunk into model
+            model.dm.addToDM(aceChunk)
+        }
+    }
+    
+    // Function that runs when there are multiple legals
+    func multipleLegals(topCard: Card) -> String {
+        
+        // Set goal state, if two was played, and if ace is in hand
+        model.buffers["goal"]!.setSlot(slot: "state", value: "multipleProcedure")
+        model.buffers["goal"]!.setSlot(slot: "two", value: twoState)
+        model.buffers["goal"]!.setSlot(slot: "ace", value: aceHand)
+        
+        // Run the model
+        model.run()
 
-			// Add ace chunk into model
-			self.model.dm.addToDm(aceChunk)
-		}	
-	}		
-	
-	// Function that runs when there are multiple legals
-	func multipleLegals() -> choice: String {
-		
-		// Set goal state, if two was played, and if ace is in hand
-		model.buffers["goal"]!.setSlot("state", "multipleProcedure")
-		model.buffers["goal"]!.setSlot("two", self.twoState)
-		model.buffers["goal"]!.setSlot("ace", self.aceHand)
-		
-		// Run the model
-		model.run()
+        // If two was not played, count legals and play accordingly
+        if model.lastAction(slot: "choice") == "countLegals" {
+            
+            // Count all legal options into a dictionary
+            var legalCounts = [String:Int]()
+            let topRank = topCard.getRank()
+            let topSuit = topCard.getSuit()
 
-		// If two was not played, count legals and play accordingly
-		if model.lastAction("choice") == "countLegals" {
-			
-			// Count all legal options into a dictionary
-			var legalCounts = [String:Int]()
-			let topRank = top.getRank().rawValue
-			let topSuit = top.getSuit().rawValue
+            for card in hand {
 
-			for card in actr.hand {
+                let rank = card.getRank()
+                let suit = card.getSuit()
 
-				let rank = card.getRank().rawValue
-				let suit = card.getSuit().rawValue
+                if rank == topRank {
+                    legalCounts[rank.rawValue]! += 1
+                } else if suit == topSuit {
+                    // Cannot play multiple suits, so always 1 if present
+                    legalCounts[suit.rawValue] = 1
+                }
+            }
+            
+            // Grab max value of dictionary
+            // Only hold suits and ranks with max count
+            var maxSuitRanks = [String]()
+            let maxSuitRank = legalCounts.values.max()
+            print("maxSuitRank: \(String(describing: maxSuitRank))")
+            for (suitRank, count) in legalCounts {
+                if (count == maxSuitRank) {
+                    maxSuitRanks.append(suitRank)
+                }
+            }
 
-				if rank == topRank {
-					legalCounts[rank] += 1
-				} else if suit == topSuit {
-					// Cannot play multiple suits, so always 1 if present
-					legalCounts[suit] = 1
-				}
-			}
-			
-			// Grab max value of dictionary
-			// Only hold suits and ranks with max count
-			var maxSuitRanks: [String]()
-			let maxSuitRank = legalCounts.max{a, b in a.rank < b.rank}
-			for (suitRank, count) in legalCounts {
-				if (count == maxSuitRank) {
-					maxSuitRanks.append(suitRank)
-				}
-			}	 
+            // If only one suitRank, then clearly a rank with more than one count
+            // Play the rank;
+            // If more than one suitRank, predictSuit and play card with suit if possible
+            if maxSuitRanks.count == 1 {
+                model.buffers["action"]!.setSlot(slot: "choice", value: "bestRank")
+                model.buffers["action"]!.setSlot(slot: "rank", value: maxSuitRanks[0])
+            } else {
+                predictSuit()
+            }
+        }
 
-			// If only one suitRank, then clearly a rank with more than one count
-			// Play the rank;
-			// If more than one suitRank, predictSuit and play card with suit if possible
-			if maxSuitRanks.count == 1 {
-				model.buffers["action"]!.setSlot("choice", "bestRank")
-				model.buffers["action"]!.setSlot("rank", maxSuitRanks[0])
-			} else {
-				model.predictSuit()
-			}
-		}
-
-		// Return the choice made
-		// If it is a prediction, grab "predict" slot from action buffer for suitRank
-		return model.lastAction("choice")
-	}
-}	
+        // Return the choice made
+        // If it is a prediction, grab "predict" slot from action buffer for suitRank
+        return model.lastAction(slot: "choice")!
+    }
+    
+    func getHand() -> [Card] {
+        return hand
+    }
+    
+    func setHand(newHand: [Card]) {
+        hand = newHand
+    }
+    
+    func getPredictedSuit() -> Suit {
+        //TODO: Might return nil
+        return suitStringToSuit(suitString: self.model.lastAction(slot: "predict")!)
+    }
+    
+    func getBestRank() -> Rank {
+        return rankStringToRank(rankString: model.lastAction(slot: "rank")!)
+    }
+    
+    func getLastAction() -> String? {
+        return self.model.lastAction(slot: "predict")
+    }
+}
