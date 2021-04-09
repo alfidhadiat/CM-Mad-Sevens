b7c92ffeb86b2a595f78ed53940ab5d4c033397a
davidkovacs <davidkovacs94@yahoo.com>
2021-04-09 11:25:12 -0700

    UI - added comments to view controller explaining each segment

--- MADSevens/MADSevens/Controller/ViewController.swift
+++ MADSevens/MADSevens/Controller/ViewController.swift
@@ -28,9 +28,6 @@ class MADSevensViewController: UIViewController {
     @IBOutlet weak var PlayerWinsLabel: UILabel!
     @IBOutlet weak var ModelWinsLabel: UILabel!
     
-//    @IBOutlet weak var rankField: UITextField!
-//    @IBOutlet weak var suitField: UITextField!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         let playerCards = game.getPlayerHand()
@@ -38,36 +35,35 @@ class MADSevensViewController: UIViewController {
         let discardCard = game.getTopDiscardCard()
         print("This is a dicard card \(discardCard)")
         
+        // display the dealt cards for the player
         for i in 0 ..< playerCards.count {
             playerCardView[i].isFaceUp = true
             playerCardView[i].setRank(newRank: playerCards[i].getRank())
             playerCardView[i].setSuit(newSuit: playerCards[i].getSuit())
         }
-        
+        // display the dealt cards for the model
         for i in 0 ..< modelCards.count {
             modelCardView[i].isFaceUp = false
             modelCardView[i].setRank(newRank: modelCards[i].getRank())
             modelCardView[i].setSuit(newSuit: modelCards[i].getSuit())
         }
-        
+        // diplay the card on top of the discard pile
         discardCardView.isFaceUp = true
         discardCardView.setRank(newRank: discardCard.getRank())
         discardCardView.setSuit(newSuit: discardCard.getSuit())
         
+        // setting up the color label; initially show the labe displaying the starting suit
         ColorLabel.isHidden = false
         ColorLabel.text = SuitToString(suitString: game.getCurrentSuit())
         ColorChoiceStack.isHidden = true
         
-        if discardCard.getRank() == Rank.VII{
-            ColorChoiceStack.isHidden = true
-            ColorLabel.isHidden = false
-            ColorLabel.text = SuitToString(suitString: game.getCurrentSuit())
-        }
-        
+        // setting up both the player and model card stacks
         playerStack.distribution = UIStackView.Distribution.equalSpacing
         modelStack.distribution = UIStackView.Distribution.equalSpacing
+        // setting up the final labels which only get displayed when one of the players win
         PlayerWinsLabel.isHidden = true
         ModelWinsLabel.isHidden = true
+        // setting up and customizing the draw card button
         DrawCardButton.backgroundColor = UIColor.darkGray
         DrawCardButton.layer.cornerRadius = DrawCardButton.frame.width / 2
         DrawCardButton.setTitleColor(UIColor.white, for: .normal)
@@ -76,6 +72,7 @@ class MADSevensViewController: UIViewController {
         DrawCardButton.layer.shadowOpacity = 0.5
         DrawCardButton.layer.shadowOffset = CGSize(width: 0, height: 0)
         
+        // setting up and customizing the end turn button
         EndTurnButton.backgroundColor = UIColor.lightGray
         EndTurnButton.layer.cornerRadius = EndTurnButton.frame.height / 2
         EndTurnButton.setTitleColor(UIColor.white, for: .normal)
@@ -85,6 +82,7 @@ class MADSevensViewController: UIViewController {
         EndTurnButton.layer.shadowOffset = CGSize(width: 0, height: 0)
         EndTurnButton.isHidden = true
         
+        // customizing the color label
         ColorLabel.backgroundColor = UIColor.systemBlue
         ColorLabel.layer.cornerRadius = EndTurnButton.frame.height / 2
         ColorLabel.layer.shadowColor = UIColor.darkGray.cgColor
@@ -92,6 +90,7 @@ class MADSevensViewController: UIViewController {
         ColorLabel.layer.shadowOpacity = 0.5
         ColorLabel.layer.shadowOffset = CGSize(width: 0, height: 0)
         
+        // adding gesture recognizer to cards that are legal to play
         for cardview in playerCardView {
             let currentCard = Card(suit: cardview.suit, rank: cardview.rank)
             if game.legalMove(card: currentCard) {
@@ -100,17 +99,26 @@ class MADSevensViewController: UIViewController {
         }
     }
     
+    // Setting up the color switching buttons.
+    // When one of the color switches is pressed the suit is set to the chosen suit, passes the turn to the model
+    // after the model plays its turn the buttons and possibly labels relevant for the player get displayed again and
+    // gesture recognizers get added to the legal cards and removed from not legal ones.
+    // the same mechanism gets triggered by each color switching button
+    
     @IBAction func SwitchToAcorn(_ sender: UIButton) {
         ColorChoiceStack.isHidden = true
         game.setNewSuit(newsuit: Suit.Acorn)
+        // get the model hand count before the model's turn
         let count = game.getModelHand().count
         game.passTurn()
         game.modelTurn()
         checkpoint()
         DrawCardButton.isHidden = false
         ColorLabel.isHidden = true
+        // get the model hand count after the model's turn
         let newcount = game.getModelHand().count
         
+        // if the nr of cards in the model's hand decreased remove the card that has been played
         if count > newcount{
             for cardview in modelCardView {
                 let currentCard = Card(suit: cardview.suit, rank: cardview.rank)
@@ -122,7 +130,7 @@ class MADSevensViewController: UIViewController {
             }
         }
 
-        
+        // if the nr of cards in the model's hand increased add the new cards to the model's hand
         if count < newcount {
             for i in 1...(newcount-count) {
                 let newCardView = ModelCardView()
@@ -138,12 +146,15 @@ class MADSevensViewController: UIViewController {
             }
         }
         
+        // the distribution of how the cards are layed out changes depending on the nr of current cards
         if self.game.getModelHand().count > 4 {
             self.modelStack.distribution = UIStackView.Distribution.fillEqually
         } else {
             self.modelStack.distribution = UIStackView.Distribution.equalSpacing
         }
         
+        // update the deck's view and display the played card
+        DeckView[0].state = game.getDeckState()
         discardCardView.rank = game.getTopDiscardCard().getRank()
         discardCardView.suit = game.getTopDiscardCard().getSuit()
         
@@ -159,6 +170,7 @@ class MADSevensViewController: UIViewController {
             }
         }
         
+        // if the model played a card and that card is a seven display the color label
         if count > game.getModelHand().count {
             if game.getTopDiscardCard().getRank() == Rank.VII {
                 ColorLabel.isHidden = false
@@ -167,6 +179,7 @@ class MADSevensViewController: UIViewController {
             }
         }
         
+        // if the model played a card and that card is a two only add gesture recognizers to the legal cards
         if discardCardView.rank == Rank.II  {
             if count > newcount {
                 for cardview in playerCardView {
@@ -229,6 +242,7 @@ class MADSevensViewController: UIViewController {
             self.modelStack.distribution = UIStackView.Distribution.equalSpacing
         }
         
+        DeckView[0].state = game.getDeckState()
         discardCardView.rank = game.getTopDiscardCard().getRank()
         discardCardView.suit = game.getTopDiscardCard().getSuit()
             
@@ -315,6 +329,7 @@ class MADSevensViewController: UIViewController {
             self.modelStack.distribution = UIStackView.Distribution.equalSpacing
         }
         
+        DeckView[0].state = game.getDeckState()
         discardCardView.rank = game.getTopDiscardCard().getRank()
         discardCardView.suit = game.getTopDiscardCard().getSuit()
         
@@ -402,6 +417,7 @@ class MADSevensViewController: UIViewController {
             self.modelStack.distribution = UIStackView.Distribution.equalSpacing
         }
         
+        DeckView[0].state = game.getDeckState()
         discardCardView.rank = game.getTopDiscardCard().getRank()
         discardCardView.suit = game.getTopDiscardCard().getSuit()
         
@@ -446,6 +462,12 @@ class MADSevensViewController: UIViewController {
     }
     
     
+    // When the end turn button is pressed the following takes place: the model's turn starts
+    // the player relevant buttons and labels get hidden then displayed accordingly.
+    // depending on the model's move a card is removed from or added to the model's hand
+    // the player's hand gets "refreshed" based on the action of the model, meaning that gesture recognizers get
+    // added to legal cards and removed from not legal ones.
+    
     @IBAction func PassTurn(_ sender: UIButton) {
         sender.isHidden = true
         let count = game.getModelHand().count
@@ -489,6 +511,7 @@ class MADSevensViewController: UIViewController {
             self.modelStack.distribution = UIStackView.Distribution.equalSpacing
         }
         
+        DeckView[0].state = game.getDeckState()
         discardCardView.rank = game.getTopDiscardCard().getRank()
         discardCardView.suit = game.getTopDiscardCard().getSuit()
         
@@ -529,11 +552,11 @@ class MADSevensViewController: UIViewController {
         DrawCardButton.isHidden = false
     }
     
+    // when the draw card button is pressed the following events take place:
+    // player gets dealt the right amount of cards (fx if a 2 is played by the model and the player has no response
+    // two cards get drawn). After this the turn gets passed to the model, and the model's hand and the top of the discard
+    // pile are also updated based on the card played by the model.
     
-    /**
-    Draws a card for the current player, gives the turn to its opponent.
-     TODO: Do we want to enable/disable this button whenever its the models turn?
-     */
     @IBAction func drawCard(_ sender: UIButton) {
         // draw card, add it to hand
         // give turn to next player
@@ -621,12 +644,6 @@ class MADSevensViewController: UIViewController {
         discardCardView.rank = game.getTopDiscardCard().getRank()
         discardCardView.suit = game.getTopDiscardCard().getSuit()
         
-        if game.getTopDiscardCard().getRank() == Rank.VII {
-            ColorLabel.isHidden = false
-            ColorLabel.text = SuitToString(suitString: game.getCurrentSuit())
-            ColorChoiceStack.isHidden = true
-        }
-        
         if discardCardView.rank == Rank.II  {
             if count > newCount {
                 for cardview in playerCardView {
@@ -646,8 +663,18 @@ class MADSevensViewController: UIViewController {
             }
         }
         sender.isHidden = false
+        if game.getTopDiscardCard().getRank() == Rank.VII {
+            ColorLabel.isHidden = false
+            ColorLabel.text = SuitToString(suitString: game.getCurrentSuit())
+            ColorChoiceStack.isHidden = true
+        }
     }
     
+    // Function for animating card movement, called by the gesture recognizer added to the player's cards
+    // Based on previous codes, the only cards tappable are legal moves. As a first step the tapped legal card view
+    // is transfered to the discard card view and gets displayed there. As soon as this animation finishes, hide the buttons
+    // and labels which are not relevant after a card has already been played. Based on the played card refresh the player's hand
+    // and add gesture recognizer to those cards that match the rank of the played card, enabling a possible sequence of cards.
     
     @objc func moveCard(_ recognizer: UITapGestureRecognizer) {
         switch recognizer.state {
